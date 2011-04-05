222c0b92a77bc3f249cfcf7cf8e789e9f9bb89e5
phpbbturkiye <admin@phpbbturkey.com>
2011-04-05 03:46:34 +0300

    - /tr/acp/board.php dosyasındaki bazı hatalar düzeltildi. - /tr/search.php dosyasındaki büyük harfler düzeltildi. - /tr/search.php dosyasındaki hatalı çeviri düzeltildi. Daha fazla bilgi: http://www.phpbbturkey.com/forums/viewtopic.php?p=100286#p100286
    Signed-off-by: phpbbturkiye <admin@phpbbturkey.com>

--- lang_phpBB_tr/language/tr/search.php
+++ lang_phpBB_tr/language/tr/search.php
@@ -37,7 +37,7 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'ALL_AVAILABLE'			=> 'Tümü',
-	'ALL_RESULTS'			=> 'Tüm Sonuçlar',
+	'ALL_RESULTS'			=> 'Tüm sonuçlar',
 
 	'DISPLAY_RESULTS'		=> 'Görüntülenecek sonuçlar',
 
@@ -80,13 +80,13 @@ $lang = array_merge($lang, array(
 	'SEARCH_AUTHOR_EXPLAIN'	=> 'Kısmen uyan sonuçlar için * işaretini joker olarak kullanabilirsiniz.',
 	'SEARCH_FIRST_POST'		=> 'Sadece başlığın ilk mesajı',
 	'SEARCH_FORUMS'			=> 'Forumlarda ara',
-	'SEARCH_FORUMS_EXPLAIN'	=> 'Arama yapmak istediğiniz forum veya forumları seçin. Eğer aşağıdaki “altforumları ara“ özelliğini kapatmazsanız altforumlar otomatik olarak aranacaktır.',
+	'SEARCH_FORUMS_EXPLAIN'	=> 'Arama yapmak istediğiniz forum veya forumları seçin. Eğer aşağıdaki “altforumlarda ara“ özelliğini kapatmazsanız altforumlar otomatik olarak aranacaktır.',
 	'SEARCH_IN_RESULTS'		=> 'Bu sonuçlarda ara',
 	'SEARCH_KEYWORDS_EXPLAIN'	=> '<strong>+</strong> ile sonuçlarda ayrıca olmasını istediğiniz kelimeleri, <strong>-</strong> ile sonuçlarda ayrıca olmasını istemediğiniz kelimeleri ve <strong>|</strong> ile sonuçlarda sadece olması gereken kelimeleri tanımlayabilirsiniz. Kısmen uyan sonuçlar için * işaretini joker olarak kullanabilirsiniz.',
 	'SEARCH_MSG_ONLY'		=> 'Sadece mesaj içeriği',
 	'SEARCH_OPTIONS'		=> 'Arama seçenekleri',
 	'SEARCH_QUERY'			=> 'Arama sorgusu',
-	'SEARCH_SUBFORUMS'		=> 'Alt forumlarda ara',
+	'SEARCH_SUBFORUMS'		=> 'Altforumlarda ara',
 	'SEARCH_TITLE_MSG'		=> 'Başlıklar ve mesaj içeriği',
 	'SEARCH_TITLE_ONLY'		=> 'Sadece konu başlıkları',
 	'SEARCH_WITHIN'			=> 'Aranacak alan',
@@ -94,10 +94,10 @@ $lang = array_merge($lang, array(
 	'SORT_AUTHOR'			=> 'Yazar',
 	'SORT_DESCENDING'		=> 'Azalan',
 	'SORT_FORUM'			=> 'Forum',
-	'SORT_POST_SUBJECT'		=> 'Mesaj Başlığı',
-	'SORT_TIME'				=> 'Mesaj Zamanı',
+	'SORT_POST_SUBJECT'		=> 'Mesaj başlığı',
+	'SORT_TIME'				=> 'Mesaj zamanı',
 
-	'TOO_FEW_AUTHOR_CHARS'	=> 'Yazar adını %d karakterden düşük belirlemelisiniz.',
+	'TOO_FEW_AUTHOR_CHARS'	=> 'Yazar adını en az %d karakter olarak belirlemelisiniz.',
 ));
 
 ?>
